cfaa0eadd1d801b8e7340bfc30a61a764b858e9d
charles gigi <charles.ginane@gmail.com>
2023-07-28 18:44:09 +0200

    Added logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "camera.hpp"
+#include "logger.hpp"
 
 int main()
 {
-    std::cout << "sncf\n";
+    auto _logger = logger::Logger("log.txt", 4, true);
+    _logger.add_log(0, "test_logger");
     auto cam = camera(0);
     auto test = cam.open();
 
